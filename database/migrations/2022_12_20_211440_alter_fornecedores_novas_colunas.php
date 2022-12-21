c4bcf8b1ad86ca904b06b234aef50c0e1e722881
junnydpetra <junnycmenezes@gmail.com>
2022-12-21 11:30:27 -0300

    Adições e alterações em Migrations

--- database/migrations/2022_12_20_211440_alter_fornecedores_novas_colunas.php
+++ database/migrations/2022_12_20_211440_alter_fornecedores_novas_colunas.php
@@ -27,6 +27,9 @@ class AlterFornecedoresNovasColunas extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('fornecedores', function (Blueprint $table)
+        {
+            $table->dropColumn(['uf','email']);
+        });
     }
 }
